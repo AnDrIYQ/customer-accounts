6a2cd630cdb5c5a51fe3ee301b813d4d009e13ec
Yurchuk <andrii.yurchuk@splynx.com>
2022-05-07 15:11:35 +0300

    CA-1 Created entry point on app #time 30m coding

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
+
+const app = require('./app/index');
+
+// APP
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Application started in port ${process.env.APP_PORT}`)
+})
+
+// API
